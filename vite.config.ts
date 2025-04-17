a4c0cba82a681fefe9856f4d6f0057410b6ed1ad
zhangchao <zhangchao@tongyuan.cc>
2025-04-17 15:49:26 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,22 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig, loadEnv } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd())
+  return {
+    server: {
+      port: parseInt(env.VITE_PORT) || 3000,
+    },
+    plugins: [vue(), vueJsx(), vueDevTools()],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
+    },
+  }
+})
