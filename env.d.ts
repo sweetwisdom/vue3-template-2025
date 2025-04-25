3173adea39f0300a819210ee32a41f684299e793
zhangchao <zhangchao@tongyuan.cc>
2025-04-25 15:46:24 +0800

    增加路由

--- env.d.ts
+++ env.d.ts
@@ -1,4 +1,5 @@
 /// <reference types="vite/client" />
+/// <reference types="unplugin-vue-router/client" />
 
 interface ImportMetaEnv {
   VITE_API_URL: string
--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
+import VueRouter from 'unplugin-vue-router/vite'
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd())
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => {
     server: {
       port: parseInt(env.VITE_PORT) || 3000,
     },
-    plugins: [vue(), vueJsx(), vueDevTools()],
+    plugins: [VueRouter(), vue(), vueJsx(), vueDevTools()],
     resolve: {
       alias: {
         '@': fileURLToPath(new URL('./src', import.meta.url)),
